a1e1346c2d9d7be794a6c38f6d09f0c85181c9ad
Vishal Yadav <vishalydv23@gmail.com>
2017-05-25 15:03:33 +0100

    login page validations

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,32 @@
 $(document).ready(function(){
 
   $('#submitButton').click(function(){
-        $.ajax({
-        type: 'POST',
-        data: 'username=' + $('#username').val() + '&password=' + $('#password').val(),
-        contentType: "application/json",
-        dataType:'json',
-        async:false,
-        url: 'http://localhost:8080/dashboard.html',                      
-        success: function(response) {
-            alert(response);
-            // console.log(response);
-            // console.log(JSON.stringify(data));                               
-        },
-        error: function(error) {
-            console.log("some error in fetching the notifications");
-         }
 
-    });
+        if( !$('#username').val() ) { //if it is blank. 
+          $('#authentication-warning').show();
+          $('#authentication-warning').text("Username can't be empty");   
+        }else if(!$('#password').val()){
+          $('#authentication-warning').show();
+          $('#authentication-warning').text("password can't be empty");
+        }else{
+            $('#authentication-warning').hide();
+            
+            $.ajax({
+                type: 'POST',
+                data: 'username=' + $('#username').val() + '&password=' + $('#password').val(),
+                contentType: "application/json",
+                dataType:'json',
+                async:false,
+                url: 'http://localhost:8080/dashboard.html',                      
+                success: function(response) {
+                    alert(response);
+                    // console.log(response);
+                    // console.log(JSON.stringify(data));                               
+                },
+                error: function(error) {
+                    console.log("some error in fetching the notifications");
+                 }
+            });
+        }
   });
-
 });
